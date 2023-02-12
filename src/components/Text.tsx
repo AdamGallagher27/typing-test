e43838a9ca15e0e2154c44b92aaf90cd1a0220d0
Adam <N00211418@iadt.ie>
2023-02-12 20:59:51 +0000

    added current letter / comp letter functions

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -3,11 +3,26 @@ import {useState} from 'react'
 export const Text = () => {
 
   const [typed, setTyped] = useState('')
+  const copy = 'blah blah blah blah'
+
+  const currentLetter = (text: string): string => {
+    return text.charAt(text.length-1)
+  }
+
+  const compLetters = (current: string, copyLetter: string): boolean  => {
+    return current === copyLetter
+  }
 
   return (
     <>
       <div>{typed}</div>
-      <input type="text" onChange={ (e) => setTyped(e.target.value)} />
+      <input type="text" onChange={ (e) => {
+        const current = currentLetter(e.target.value)
+        setTyped(e.target.value)
+        console.log(current)
+        // console.log(currentLetter(e.target.value))
+        // console.log(compLetters(e.target.value, 'p'))
+      }} />
     </>
   )
 }
