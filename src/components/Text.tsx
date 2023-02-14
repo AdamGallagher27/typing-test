1ccd666ffc0beabf43a42928096ef9ab6c510e09
Adam <N00211418@iadt.ie>
2023-02-14 21:22:07 +0000

    added functionality to compare letters

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,27 +1,38 @@
 import {useState} from 'react'
 
+
+
 export const Text = () => {
 
   const [typed, setTyped] = useState('')
   const copy = 'blah blah blah blah'
+
 
   const currentLetter = (text: string): string => {
     return text.charAt(text.length-1)
   }
 
   const compLetters = (current: string, copyLetter: string): boolean  => {
-    return current === copyLetter
+    if(current == copyLetter) {
+      console.log(current + ' is ' + copyLetter)
+      return true
+    }
+    else {
+      console.log(current + ' is not ' + copyLetter)
+      return false
+    }
   }
 
   return (
     <>
+      <div>{copy}</div>
       <div>{typed}</div>
       <input type="text" onChange={ (e) => {
         const current = currentLetter(e.target.value)
+        const position = e.target.value.length - 1
         setTyped(e.target.value)
-        console.log(current)
-        // console.log(currentLetter(e.target.value))
-        // console.log(compLetters(e.target.value, 'p'))
+        compLetters(current, copy[position])
+
       }} />
     </>
   )
