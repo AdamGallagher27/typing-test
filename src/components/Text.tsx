282cdc993be5199b8ed49e672bd3b2ec476f4b40
Adam <N00211418@iadt.ie>
2023-05-09 14:12:29 +0100

    added comments

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,39 +1,40 @@
-import {useState} from 'react'
+import { useState } from 'react'
 
 
 
 export const Text = () => {
 
-  const [typed, setTyped] = useState('')
-  const copy = 'blah blah blah blah'
+	const [typed, setTyped] = useState('')
+	const copy = 'blah blah blah blah'
 
+	// returns the current letter
+	const currentLetter = (text: string): string => {
+		return text.charAt(text.length - 1)
+	}
 
-  const currentLetter = (text: string): string => {
-    return text.charAt(text.length-1)
-  }
+	// checks if the current letter is === copyLetter
+	const compLetters = (current: string, copyLetter: string): boolean => {
+		if (current == copyLetter) {
+			console.log(current + ' is ' + copyLetter)
+			return true
+		}
+		else {
+			console.log(current + ' is not ' + copyLetter)
+			return false
+		}
+	}
 
-  const compLetters = (current: string, copyLetter: string): boolean  => {
-    if(current == copyLetter) {
-      console.log(current + ' is ' + copyLetter)
-      return true
-    }
-    else {
-      console.log(current + ' is not ' + copyLetter)
-      return false
-    }
-  }
+	return (
+		<>
+			<div>{copy}</div>
+			<div>{typed}</div>
+			<input type="text" onChange={(e) => {
+				const current = currentLetter(e.target.value)
+				const position = e.target.value.length - 1
+				setTyped(e.target.value)
+				compLetters(current, copy[position])
 
-  return (
-    <>
-      <div>{copy}</div>
-      <div>{typed}</div>
-      <input type="text" onChange={ (e) => {
-        const current = currentLetter(e.target.value)
-        const position = e.target.value.length - 1
-        setTyped(e.target.value)
-        compLetters(current, copy[position])
-
-      }} />
-    </>
-  )
+			}} />
+		</>
+	)
 }
