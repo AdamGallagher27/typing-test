ae03972807edd2d1a5e2cd694c61ecdc5e00b3a2
Adam <N00211418@iadt.ie>
2023-05-11 18:25:34 +0100

    added gameover/words state variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import { Text } from './components/Text';
-import { Timer } from './components/Timer';
-
+import React, { useState, useEffect } from "react";
+import { Timer } from "./components/Timer";
+import { Text } from "./components/Text";
 
 function App() {
-  return (
-    <div>
-      <Timer />
-      <Text />
-    </div>
-  );
+    const [words, setWords] = useState(null);
+    const [gameOver, setGameOver] = useState(false);
+
+    return (
+        <div>
+            <p> {gameOver ? "true" : "false"} </p>
+            <p>{words}</p>
+            <Text setWords={setWords} gameOver={gameOver} />
+            <Timer setGameOver={setGameOver} />
+        </div>
+    );
 }
 
 export default App;
