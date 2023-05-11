9b6f3d2fd3392f0ae8bc4aff822d05508f15eb47
Adam <N00211418@iadt.ie>
2023-05-11 18:22:36 +0100

    added props / set game over after countdown

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 
-export const Timer = () => {
+interface Props {
+    setGameOver: React.Dispatch<React.SetStateAction<any>>,
+};
+
+
+export const Timer = (props: Props) => {
 
 	// start time and state variable for the timer
-	const startTime = 60
+	const startTime = 5
 	const [time, setTime] = useState(startTime)
 
 	// reset the timer to the start variable
@@ -16,12 +21,15 @@ export const Timer = () => {
 	// if time is 0 call reset
 	const tick = () => {
 		if(time === 0) {
+			props.setGameOver(true)
 			resetTimer()
 		}
 		else {
 			setTime( time - 1 )
 		}
 	}
+
+
 
 	// every 1000 miliseconds call tick then clear interval
 	useEffect(() => {
