ee0080d09c29a5b6deebc0684c043ca98a3c313d
Adam <N00211418@iadt.ie>
2023-05-17 14:27:37 +0100

    finished functionality mvp

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
+import '../App.css'
 
 interface Props {
 	setWords: React.Dispatch<React.SetStateAction<any>>,
@@ -9,24 +10,71 @@ interface Props {
 export const Text = (props: Props) => {
 
 	const copy = props.copy
+	const inputRef = useRef<HTMLInputElement>(null)
 	const [typed, setTyped] = useState('')
+
+
+	// focus on the text input box
+	useEffect(() => {
+		inputRef.current?.focus()
+	}, [])
+
 
 	// returns the current letter
 	const currentLetter = (text: string): string => {
 		return text.charAt(text.length - 1)
 	}
 
-	// checks if the current letter is === copyLetter
-	const compLetters = (current: string, copyLetter: string): boolean => {
-		if (current == copyLetter) {
-			console.log(current + ' is ' + copyLetter)
-			return true
+
+	// this splits copy text into charachters wrapped by spans
+	// with unique id
+	const renderCopy = (copy: string) => {
+		const chars = copy.split('');
+		return chars.map((char, index) => (
+			<span className='reset' id={index.toString()} key={index}>{char}</span>
+		))
+	}
+
+
+	// add a class to a specified span
+	const targetSpan = (id: string, className: string) => {
+		const span = document.getElementById(id)
+		span?.classList.add(className)
+	}
+
+
+	// remove classes on span
+	const resetCol = (id: string) => {
+		const span = document.getElementById(id)
+		
+		if(span?.classList.contains('right')) {
+			span?.classList.remove('right')
 		}
-		else {
-			console.log(current + ' is not ' + copyLetter)
-			return false
+		if(span?.classList.contains('wrong')) {
+			span?.classList.remove('wrong')
 		}
 	}
+
+
+	// checks if the current letter is === copyLetter
+	const compLetters = (current: string, index: number) => {
+
+		const copyLetter = copy[index]
+
+		// reset the colour for the charachter ahead of current index
+		resetCol((index + 1).toString())
+
+		// if the typed charchter is correct make it blue
+		// if its wrong make it red
+		if (current == copyLetter) {
+			targetSpan(index.toString(), 'right')
+		}
+		else if (current != copyLetter) {
+			targetSpan(index.toString(), 'wrong')
+		}
+
+	}
+
 
 	// when the timer runs out and game over becomes true
 	// send typed words to parent component
@@ -36,21 +84,23 @@ export const Text = (props: Props) => {
 		}
 	}
 
+
 	return (
 		<>
-			<div>{copy}</div>
+			<div>{renderCopy(props.copy)}</div>
 			{
 				// if game is not over show input
 				!props.gameOver ?
-					<input type="text" onChange={(e) => {
+					<input type="text" ref={inputRef} onChange={(e) => {
 						const current = currentLetter(e.target.value)
 						const position = e.target.value.length - 1
+						// console.log(position)
 						setTyped(e.target.value)
-						compLetters(current, copy[position])
-					}} /> 
-				
-				// else send sentence to 
-				: getSentence(typed)
+						compLetters(current, position)
+					}} />
+
+					// else send sentence to 
+					: getSentence(typed)
 			}
 		</>
 	)
